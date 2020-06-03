d533a55adedb66e6493125420153f440e4e18f6f
winesoft <service1@winesoft.co.kr>
2020-06-03 19:11:43 +0900

    1
    1

--- conf.py
+++ conf.py
@@ -46,7 +46,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'CSP'
+project = 'Contents Service Patterns'
 copyright = '2020, WineSOFT Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
